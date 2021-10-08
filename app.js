9f5f138f6be910fdc83e8b0c7ac6f99cc57c78c9
Corrie C. Graham <corrieg@ornico.co.za>
2021-10-08 13:39:51 +0200

    chore: added env variables

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs } = require('./schema/TypeDefs');
 const { resolvers } = require('./schema/Resolvers');
 
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+require('dotenv').config();
+
+const app = express();
+const __port__ = process.env.APP_PORT;
 
 const startServer = async () => {
   const apolloServer = new ApolloServer({ typeDefs, resolvers });
@@ -21,8 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.listen(3001, () => {
-  console.log('SERVER IS RUNNING ON PORT 3001');
+app.listen(__port__, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${__port__}`);
 });
 
 module.exports = app;
